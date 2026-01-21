4b02f19704c38b14b7be84765dc8cae588bd1509
Slavi <s_gatev@yahoo.com>
2026-01-21 16:58:15 +0200

    When changing stock locations quantity can not be negative.

--- warehouse-management/src/main/java/app/stock/service/StockService.java
+++ warehouse-management/src/main/java/app/stock/service/StockService.java
@@ -4,6 +4,7 @@ import app.locations.model.Locations;
 import app.locations.service.LocationService;
 import app.stock.model.Stock;
 import app.stock.repository.StockRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -21,38 +22,41 @@ public class StockService {
         this.locationService = locationService;
     }
 
-
     public void saveStock(Stock stock) {
         stockRepository.save(stock);
     }
 
-
+    @Transactional
     public void changeStockLocations(Long stockId, BigDecimal quantity, String locationCode) {
 
         Optional<Stock> optionalStock = stockRepository.findStockById(stockId);
-        Stock stock = optionalStock.get();
-        stock.setQuantity(stock.getQuantity().subtract(quantity));
-        stockRepository.save(stock);
+        if (optionalStock.isPresent()) {
+            Stock stock = optionalStock.get();
+            if (stock.getQuantity().compareTo(quantity) < 0) {
+                throw new RuntimeException("Quantity you want to move is more than the stock quantity!\n Available quantity is: " + stock.getQuantity());
 
-        Locations location = locationService.findLocationByCode(locationCode);
+            } else if (stock.getQuantity().compareTo(quantity) >= 0) {
+                stock.setQuantity(stock.getQuantity().subtract(quantity));
+            }
 
-        Optional<Stock> optionalStockToMoove = stockRepository.findStockByLocation_CodeAndBatch_BatchNumber(locationCode, stock.getBatch().getBatchNumber());
+            stockRepository.save(stock);
+            Locations location = locationService.findLocationByCode(locationCode);
+            Optional<Stock> optionalStockToMove = stockRepository.findStockByLocation_CodeAndBatch_BatchNumber(locationCode, stock.getBatch().getBatchNumber());
 
-        if (optionalStockToMoove.isPresent()) {
-            Stock stockToMoove = optionalStockToMoove.get();
-            stockToMoove.setQuantity(quantity.add(stockToMoove.getQuantity()));
-            stockRepository.save(stockToMoove);
-        } else {
-            Stock moovedStock = Stock.builder()
-                    .batch(stock.getBatch())
-                    .prodName(stock.getProdName())
-                    .quantity(quantity)
-                    .build();
-            moovedStock.setLocation(location);
-            stockRepository.save(moovedStock);
+            if (optionalStockToMove.isPresent()) {
+                Stock stockToMove = optionalStockToMove.get();
+                stockToMove.setQuantity(stockToMove.getQuantity().add(quantity));
+                stockRepository.save(stockToMove);
+            } else {
+                Stock moovedStock = Stock.builder()
+                        .batch(stock.getBatch())
+                        .prodName(stock.getProdName())
+                        .quantity(quantity)
+                        .build();
+                moovedStock.setLocation(location);
+                stockRepository.save(moovedStock);
 
+            }
         }
-
     }
-
 }
