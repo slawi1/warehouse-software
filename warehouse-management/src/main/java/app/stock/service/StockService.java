fe7db94c06eb992a91c507414ba13279388cb838
Slavi <s_gatev@yahoo.com>
2026-01-29 15:05:54 +0200

    added limitaitons to stock movement

--- warehouse-management/src/main/java/app/stock/service/StockService.java
+++ warehouse-management/src/main/java/app/stock/service/StockService.java
@@ -34,33 +34,44 @@ public class StockService {
     public void changeStockLocations(Long stockId, BigDecimal quantity, String locationCode) {
 
         Optional<Stock> optionalStock = stockRepository.findStockById(stockId);
-        if (optionalStock.isPresent()) {
-            Stock stock = optionalStock.get();
-            if (stock.getQuantity().compareTo(quantity) < 0) {
-                throw new RuntimeException("Quantity you want to move is more than the stock quantity!\n Available quantity is: " + stock.getQuantity());
 
-            } else if (stock.getQuantity().compareTo(quantity) >= 0) {
-                stock.setQuantity(stock.getQuantity().subtract(quantity));
-            }
+        if (optionalStock.isEmpty()) {
+            throw new RuntimeException("Stock does not exists!");
+        }
 
-            stockRepository.save(stock);
-            Locations location = locationService.findLocationByCode(locationCode);
-            Optional<Stock> optionalStockToMove = stockRepository.findStockByLocation_CodeAndBatch_BatchNumber(locationCode, stock.getBatch().getBatchNumber());
+        Stock stock = optionalStock.get();
 
-            if (optionalStockToMove.isPresent()) {
-                Stock stockToMove = optionalStockToMove.get();
-                stockToMove.setQuantity(stockToMove.getQuantity().add(quantity));
-                stockRepository.save(stockToMove);
-            } else {
-                Stock moovedStock = Stock.builder()
-                        .batch(stock.getBatch())
-                        .prodName(stock.getProdName())
-                        .quantity(quantity)
-                        .build();
-                moovedStock.setLocation(location);
-                stockRepository.save(moovedStock);
+        if (stock.getLocation().getCode().equals(locationCode)) {
+            throw new RuntimeException("New location address must be different from the current!");
+        }
 
-            }
+        if (stock.getQuantity().compareTo(quantity) < 0) {
+            throw new RuntimeException("Quantity you want to move is more than the stock quantity!\n Available quantity is: " + stock.getQuantity());
+
+        } else if (stock.getQuantity().compareTo(quantity) >= 0) {
+            stock.setQuantity(stock.getQuantity().subtract(quantity));
+        }
+
+        stockRepository.save(stock);
+        Locations location = locationService.findLocationByCode(locationCode);
+        Optional<Stock> optionalStockToMove = stockRepository.findStockByLocation_CodeAndBatch_BatchNumber(locationCode, stock.getBatch().getBatchNumber());
+
+        if (optionalStockToMove.isPresent()) {
+            Stock stockToMove = optionalStockToMove.get();
+            stockToMove.setQuantity(stockToMove.getQuantity().add(quantity));
+            stockRepository.save(stockToMove);
+        } else {
+            Stock moovedStock = Stock.builder()
+                    .batch(stock.getBatch())
+                    .prodName(stock.getProdName())
+                    .quantity(quantity)
+                    .build();
+            moovedStock.setLocation(location);
+            stockRepository.save(moovedStock);
+
+        }
+        if (stock.getQuantity().compareTo(BigDecimal.ZERO) == 0) {
+            stockRepository.delete(stock);
         }
     }
 
